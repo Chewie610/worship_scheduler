90696268961b92b9140f67858fd4b2704d634765
Chewie610 <105380641+Chewie610@users.noreply.github.com>
2024-12-11 14:49:58 -0500

    Update formdata.js
    Added dummy code for testing

--- formdata.js
+++ formdata.js
@@ -1,9 +1,13 @@
 function onFormSubmit(e) {
   // Get the submitted values from the form
-  var submittedData = e.values;  // e.values is an array containing the form responses
+  //var submittedData = e.values;  // e.values is an array containing the form responses
   
   // Get the sheet where the form responses are saved
-  var sheet = e.source.getSheetByName('Form Responses'); // Replace with your sheet name
+  //var sheet = e.source.getSheetByName('Form Responses'); // Replace with your sheet name
+
+  //Since we want to just test our code, use this to get values for now:
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Form Responses');
+  var submittedData = sheet.getRange('A2:N2').getValues();  // Get values from A1 to F2 (returns a 2D array)
 
   //Access the values
   var name = submittedData[1];
